a707a3cf38e07c75006ea9cd6455cb90a76367ab
Administrator <Administrator@ADMINISTRATOR>
2022-11-03 17:27:29 +0700

    add BiZhong Form

---
+++ LiuShuiZhang2.0/BiZhong.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace LiuShuiZhang2._0
+{
+    partial class BiZhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_User = new System.Windows.Forms.Panel();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox_Disable = new System.Windows.Forms.CheckBox();
+            this.dataGridView_User = new System.Windows.Forms.DataGridView();
+            this.button_Update = new System.Windows.Forms.Button();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox_RePassWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_PassWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.panel_User.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_User)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_User
+            // 
+            this.panel_User.Controls.Add(this.button_Clear);
+            this.panel_User.Controls.Add(this.label6);
+            this.panel_User.Controls.Add(this.label5);
+            this.panel_User.Controls.Add(this.checkBox_Disable);
+            this.panel_User.Controls.Add(this.dataGridView_User);
+            this.panel_User.Controls.Add(this.button_Update);
+            this.panel_User.Controls.Add(this.button_Add);
+            this.panel_User.Controls.Add(this.radioButton5);
+            this.panel_User.Controls.Add(this.radioButton4);
+            this.panel_User.Controls.Add(this.radioButton3);
+            this.panel_User.Controls.Add(this.radioButton2);
+            this.panel_User.Controls.Add(this.label4);
+            this.panel_User.Controls.Add(this.radioButton1);
+            this.panel_User.Controls.Add(this.textBox_RePassWord);
+            this.panel_User.Controls.Add(this.label3);
+            this.panel_User.Controls.Add(this.textBox_PassWord);
+            this.panel_User.Controls.Add(this.label2);
+            this.panel_User.Controls.Add(this.textBox_UserName);
+            this.panel_User.Controls.Add(this.label1);
+            this.panel_User.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_User.Location = new System.Drawing.Point(0, 0);
+            this.panel_User.Name = "panel_User";
+            this.panel_User.Size = new System.Drawing.Size(584, 555);
+            this.panel_User.TabIndex = 0;
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Clear.ForeColor = System.Drawing.Color.Red;
+            this.button_Clear.Location = new System.Drawing.Point(510, 214);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(62, 35);
+            this.button_Clear.TabIndex = 15;
+            this.button_Clear.Text = "刷新";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(16, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "种類";
+            // 
+            // checkBox_Disable
+            // 
+            this.checkBox_Disable.AutoSize = true;
+            this.checkBox_Disable.Location = new System.Drawing.Point(109, 194);
+            this.checkBox_Disable.Name = "checkBox_Disable";
+            this.checkBox_Disable.Size = new System.Drawing.Size(15, 14);
+            this.checkBox_Disable.TabIndex = 13;
+            this.checkBox_Disable.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView_User
+            // 
+            this.dataGridView_User.AllowUserToAddRows = false;
+            this.dataGridView_User.AllowUserToDeleteRows = false;
+            this.dataGridView_User.AllowUserToResizeColumns = false;
+            this.dataGridView_User.AllowUserToResizeRows = false;
+            this.dataGridView_User.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_User.Location = new System.Drawing.Point(20, 255);
+            this.dataGridView_User.MultiSelect = false;
+            this.dataGridView_User.Name = "dataGridView_User";
+            this.dataGridView_User.ReadOnly = true;
+            this.dataGridView_User.Size = new System.Drawing.Size(552, 293);
+            this.dataGridView_User.TabIndex = 12;
+            this.dataGridView_User.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_User_CellClick);
+            // 
+            // button_Update
+            // 
+            this.button_Update.Enabled = false;
+            this.button_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Update.ForeColor = System.Drawing.Color.Red;
+            this.button_Update.Location = new System.Drawing.Point(126, 214);
+            this.button_Update.Name = "button_Update";
+            this.button_Update.Size = new System.Drawing.Size(100, 35);
+            this.button_Update.TabIndex = 10;
+            this.button_Update.Text = "更改";
+            this.button_Update.UseVisualStyleBackColor = true;
+            this.button_Update.Click += new System.EventHandler(this.button_Update_Click);
+            // 
+            // button_Add
+            // 
+            this.button_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Add.ForeColor = System.Drawing.Color.Red;
+            this.button_Add.Location = new System.Drawing.Point(20, 214);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(100, 35);
+            this.button_Add.TabIndex = 9;
+            this.button_Add.Text = "添加";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.Red;
+            this.radioButton2.Location = new System.Drawing.Point(219, 148);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 27);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Tag = "1";
+            this.radioButton2.Text = "管理员";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(16, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "一共";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.Red;
+            this.radioButton1.Location = new System.Drawing.Point(109, 148);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 27);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "2";
+            this.radioButton1.Text = "外币";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox_RePassWord
+            // 
+            this.textBox_RePassWord.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_RePassWord.Location = new System.Drawing.Point(109, 82);
+            this.textBox_RePassWord.Name = "textBox_RePassWord";
+            this.textBox_RePassWord.PasswordChar = '*';
+            this.textBox_RePassWord.Size = new System.Drawing.Size(463, 29);
+            this.textBox_RePassWord.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(16, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "平均价";
+            // 
+            // textBox_PassWord
+            // 
+            this.textBox_PassWord.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_PassWord.Location = new System.Drawing.Point(109, 47);
+            this.textBox_PassWord.Name = "textBox_PassWord";
+            this.textBox_PassWord.PasswordChar = '*';
+            this.textBox_PassWord.Size = new System.Drawing.Size(463, 29);
+            this.textBox_PassWord.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(16, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "数量";
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_UserName.Location = new System.Drawing.Point(109, 9);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(463, 29);
+            this.textBox_UserName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "币种";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(16, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "停用";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.ForeColor = System.Drawing.Color.Red;
+            this.radioButton3.Location = new System.Drawing.Point(307, 148);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(82, 27);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Tag = "1";
+            this.radioButton3.Text = "管理员";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.ForeColor = System.Drawing.Color.Red;
+            this.radioButton4.Location = new System.Drawing.Point(395, 148);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(82, 27);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.Tag = "1";
+            this.radioButton4.Text = "管理员";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.ForeColor = System.Drawing.Color.Red;
+            this.radioButton5.Location = new System.Drawing.Point(483, 148);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(82, 27);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.Tag = "1";
+            this.radioButton5.Text = "管理员";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // BiZhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 555);
+            this.Controls.Add(this.panel_User);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BiZhong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "人员";
+            this.Load += new System.EventHandler(this.User_Load);
+            this.panel_User.ResumeLayout(false);
+            this.panel_User.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_User)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_User;
+        private System.Windows.Forms.TextBox textBox_RePassWord;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_PassWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Update;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DataGridView dataGridView_User;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox_Disable;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
---
+++ LiuShuiZhang2.0/BiZhong.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiuShuiZhang2._0.BLL;
+
+namespace LiuShuiZhang2._0
+{
+    public partial class BiZhong : Form
+    {
+        
+        DAL.DAL_User DAL_User;
+       
+        public BiZhong()
+        {
+            InitializeComponent();
+            DAL_User = new DAL.DAL_User();
+        }
+
+        private void User_Load(object sender, EventArgs e)
+        {
+            dataGridView_User.DataSource = DAL_User.GetAllUser();
+            dataGridView_User.Columns["RENYUANID"].HeaderText = "人员码";
+            dataGridView_User.Columns["YONGHU"].HeaderText = "用户";
+            dataGridView_User.Columns["MIMA"].HeaderText = "密码";
+            dataGridView_User.Columns["QUAN"].HeaderText = "职位";
+            dataGridView_User.Columns["TINGYONG"].HeaderText = "停用";
+
+            dataGridView_User.Tag = false;
+            ChangeMode((bool)dataGridView_User.Tag);
+
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            if (CheckData(false))
+            {
+                if (MessageBox.Show("请确认是否要添加人员？","温卿提示",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        DAL_User.AddNewUser(new BLL_User
+                        {
+                            UserName = textBox_UserName.Text,
+                            PassWord = Common.EncodePasswordToBase64(textBox_PassWord.Text),
+                            Permission = int.Parse(((RadioButton)panel_User.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked)).Tag.ToString()),
+                            Disable = checkBox_Disable.Checked
+                        }) ;
+                        MessageBox.Show("添加人员成功", "温卿提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        ClearForm();
+                        User_Load(sender, e);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "温卿提示",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button_Update_Click(object sender, EventArgs e)
+        {
+            if (CheckData(true))
+            {
+                if (MessageBox.Show("请确认是否要更改人员信息？", "温卿提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        DAL_User.EditNewUser(new BLL_User
+                        {
+                            UserID = long.Parse(textBox_UserName.Tag.ToString()),
+                            PassWord = Common.EncodePasswordToBase64(textBox_PassWord.Text),
+                            Permission = int.Parse(((RadioButton)panel_User.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked)).Tag.ToString()),
+                            Disable = checkBox_Disable.Checked
+                        });
+
+                        MessageBox.Show("更改人员信息成功", "温卿提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ClearForm();
+                        User_Load(sender, e);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "温卿提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            User_Load(sender, e);
+            ClearForm();
+        }
+
+        private void dataGridView_User_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView_User.Tag = true;
+            ChangeMode((bool)dataGridView_User.Tag);
+
+            textBox_UserName.Tag = dataGridView_User.CurrentRow.Cells["RENYUANID"].Value.ToString();
+            textBox_UserName.Text = dataGridView_User.CurrentRow.Cells["YONGHU"].Value.ToString();
+            textBox_PassWord.Text = dataGridView_User.CurrentRow.Cells["MIMA"].Value.ToString();
+            textBox_RePassWord.Text = dataGridView_User.CurrentRow.Cells["MIMA"].Value.ToString();
+            ((RadioButton)panel_User.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Tag.ToString()==dataGridView_User.CurrentRow.Cells["QUAN"].Value.ToString())).Checked = true;
+            checkBox_Disable.Checked = ((Boolean)dataGridView_User.CurrentRow.Cells["TINGYONG"].Value);
+        }
+
+        private bool CheckData(bool isEdit)
+        {
+            bool rs = false;
+            if (!Common.IsNumber(textBox_UserName.Text) && 
+                Common.NotInludeSpecialChar(textBox_UserName.Text) &&
+                textBox_UserName.Text.Length >= 4 && textBox_UserName.Text.Length <= 16)
+                rs = true;
+            else
+            {
+                MessageBox.Show("用户名不能是数字或存在特别码或存在大写，长度要在4-16码之间，请重新输入");
+                textBox_UserName.Focus();
+                textBox_UserName.SelectAll();
+                return false;
+            }
+
+            if (!isEdit)
+            {
+                if (!DAL_User.UserNameExisted(textBox_UserName.Text))
+                    rs = true;
+                else
+                {
+                    MessageBox.Show("用户名已经存在，请选择别的");
+                    textBox_UserName.Focus();
+                    textBox_UserName.SelectAll();
+                    return false;
+                }
+            }
+
+            if (textBox_PassWord.Text == textBox_RePassWord.Text &&
+                textBox_PassWord.Text.Length >= 8 && textBox_PassWord.Text.Length <= 16)
+                rs = true;
+            else
+            {
+                MessageBox.Show("密码输入不一致，长度要在8-16码之间，请重新输入");
+                textBox_PassWord.Clear();
+                textBox_RePassWord.Clear();
+                textBox_PassWord.Focus();
+                return false;
+            }
+
+            return rs;
+        }
+
+        private void ClearForm()
+        {
+            textBox_UserName.Clear();
+            textBox_UserName.Tag = string.Empty;
+            textBox_PassWord.Clear();
+            textBox_RePassWord.Clear();
+            textBox_UserName.Focus();
+        }
+
+        private void ChangeMode(bool isEdit)
+        {
+            if (isEdit)
+            {
+                button_Add.Enabled = false;
+                button_Update.Enabled = true;
+                textBox_UserName.Enabled = false;
+            }
+            else
+            {
+                button_Add.Enabled = true;
+                button_Update.Enabled = false;
+                textBox_UserName.Enabled = true;
+            }
+        }
+    }
+}
